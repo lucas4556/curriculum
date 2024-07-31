6305ddc350eb99464bfd2f6b34c2ea4b0c01b4c8
lucas4556 <161216307+lucas4556@users.noreply.github.com>
2024-07-31 08:44:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuLateral(){
+    let menuMobile = document.querySelector('.menu-mobile')
+    let menuListMobile = document.querySelector('.menu-list-mobile')
+
+    menuMobile.classList.toggle('active')
+    menuListMobile.classList.toggle('active')
+}
